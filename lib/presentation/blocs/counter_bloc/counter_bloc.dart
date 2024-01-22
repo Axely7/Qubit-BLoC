25ca1d9babd8f8fa319358e024bcb2e7237df0f8
Axely7 <axely7@hotmail.com>
2024-01-22 13:19:21 -0600

    using actions form BLoC

--- lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -7,6 +7,7 @@ part 'counter_state.dart';
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(const CounterState()) {
     on<CounterIncreased>(_onCounterIncreased);
+    on<CounterReset>(_onCounterReset);
   }
 
   void _onCounterIncreased(
@@ -16,5 +17,9 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
     emit(state.copyWith(
         counter: state.counter + event.value,
         transactionCount: state.transactionCount + 1));
+  }
+
+  void _onCounterReset(CounterReset event, Emitter<CounterState> emit) {
+    emit(state.copyWith(counter: 0));
   }
 }
