ecefea61b1a5be11bf432befe500c89d8f2d9bda
Axely7 <axely7@hotmail.com>
2024-01-23 10:32:05 -0600

    working with forms

---
+++ lib/presentation/screens/register_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nuevo usuario"),
+      ),
+      body: const _RegisterView(),
+    );
+  }
+}
+
+class _RegisterView extends StatelessWidget {
+  const _RegisterView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlutterLogo(
+                size: 100,
+              ),
+              _RegisterForm(),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RegisterForm extends StatelessWidget {
+  const _RegisterForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      children: [
+        TextFormField(),
+        const SizedBox(
+          height: 20,
+        ),
+        FilledButton.tonalIcon(
+            onPressed: () {},
+            icon: const Icon(Icons.save),
+            label: const Text("Crear usuario")),
+      ],
+    ));
+  }
+}
